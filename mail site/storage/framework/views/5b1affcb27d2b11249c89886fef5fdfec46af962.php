c653d8ac07832392e47008d6b326d4cd7f38147f
COdeR2903 <54071220+PuneetKumar2903@users.noreply.github.com>
2022-03-27 00:52:20 +0530

    Add files via upload

---
+++ mail site/storage/framework/views/5b1affcb27d2b11249c89886fef5fdfec46af962.php
@@ -0,0 +1,8 @@
+<?php echo e($body); ?>
+
+
+<div>
+<h3>Thank you</h3>
+<h4>Vpuneet kumar</h4>
+</div>
+<?php /**PATH C:\php college\htdocs\Mail\resources\views/email-template.blade.php ENDPATH**/ ?>
